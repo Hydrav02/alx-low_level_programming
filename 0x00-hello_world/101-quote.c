17e4682d7e119a22ce2fc2ed22980e3245855df3
Hydrav02 <med.bakhkhouch@gmail.com>
2023-07-13 19:17:04 -0700

    hello c tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,13 @@
 
 /**
  * main - Entry point
- * Description: print a code using write func
- * write (int fd,const void *buf,size_t count)
+ * Description: print a code using fput func
+ * 
  * Return: 1 (fail)
  */
 
 
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(1, quo, 59);
+{	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
